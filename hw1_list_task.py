0d29804a545f312f5420536056e3fa611741376a
Konstantin Makhnev <mah.kons@gmail.com>
2017-09-06 02:57:58 +0500

    Homework1 done

--- hw1_list_task.py
+++ hw1_list_task.py
@@ -2,18 +2,39 @@
 #
 # Example input: [1, 2, 2, 3]
 # Example output: [1, 2, 3]
+
+
 def remove_adjacent(lst):
     i = 0
-    while i + 1 < len(lst)
-        
+    while i + 1 < len(lst):
+        if lst[i] == lst[i + 1]:
+            lst.pop(i + 1)
+        i += 1
+    print(lst)
     return
 
 # Merge two sorted lists in one sorted list in linear time
 #
 # Example input: [2, 4, 6], [1, 3, 5]
 # Example output: [1, 2, 3, 4, 5, 6]
+
+
 def linear_merge(lst1, lst2):
-    i = 0, j = 0
-    while i < len(lst1) and j < len(lst2)
-        
+    i = 0
+    j = 0
+    lst = []
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] < lst2[j]:
+            lst.append(lst1[i])
+            i += 1
+        else:
+            lst.append(lst2[j])
+            j += 1
+    while i < len(lst1):
+        lst.append(lst1[i])
+        i += 1
+    while j < len(lst2):
+        lst.append(lst2[j])
+        j += 1
+    print(lst)
     return
---
+++ hw1_string_task.py
@@ -0,0 +1,58 @@
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+#
+# Example input: 'read'
+# Example output: 'reading'
+
+
+def verbing(s):
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            s += 'ly'
+        else:
+            s += 'ing'
+    print(s)
+    return
+
+
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+#
+# Example input: 'This dinner is not that bad!'
+# Example output: 'This dinner is good!'
+
+
+def not_bad(s):
+    p1 = s.find('not')
+    p2 = s.find('bad')
+    if p1 != -1 and p2 != -1 and p1 < p2:
+        s = s[:p1] + 'good' + s[p2 + 3:]
+    print(s)
+    return
+
+
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+#
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+#
+# Example input: 'abcd', 'xy'
+# Example output: 'abxcdy'
+
+
+def front_back(a, b):
+    mid_a = (len(a) + 1) // 2
+    mid_b = (len(b) + 1) // 2
+    res = a[:mid_a] + b[:mid_b] + a[mid_a:] + b[mid_b:]
+    print(res)
+    return
