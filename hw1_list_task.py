d0a79b549cfbe8cd79a050d10270c5319bf5f954
Konstantin Makhnev <mah.kons@gmail.com>
2017-09-05 16:28:26 +0500

    Add empty files

---
+++ hw1_list_task.py
@@ -0,0 +1,19 @@
+# Remove equal adjacent elements
+#
+# Example input: [1, 2, 2, 3]
+# Example output: [1, 2, 3]
+def remove_adjacent(lst):
+    i = 0
+    while i + 1 < len(lst)
+        
+    return
+
+# Merge two sorted lists in one sorted list in linear time
+#
+# Example input: [2, 4, 6], [1, 3, 5]
+# Example output: [1, 2, 3, 4, 5, 6]
+def linear_merge(lst1, lst2):
+    i = 0, j = 0
+    while i < len(lst1) and j < len(lst2)
+        
+    return
