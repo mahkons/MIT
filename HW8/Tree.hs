4a192695dce836d54ce30fd9342b49ec1545334c
Konstantin <mah.kons@gmail.com>
2017-11-06 20:06:38 +0500

    Fixes

--- HW8/Tree.hs
+++ HW8/Tree.hs
@@ -1,54 +1,37 @@
 import Prelude hiding (lookup)
 
+
 data BinaryTree k v = Nil
-	| Node k v (BinaryTree k v) (BinaryTree k v) Int
+	| Node k v (BinaryTree k v) (BinaryTree k v)
 	deriving (Show, Eq, Ord)
+
 
 lookup :: Ord k => k -> BinaryTree k v -> Maybe v
 lookup k Nil = Nothing
-lookup k (Node key value nodel noder size)
+lookup k (Node key value nodel noder)
 	| key == k = Just value
-	| key < k = lookup k $ nodel
-	| otherwise = lookup k $ noder
-
-
---Balancing tree
-get_size Nil = 0
-get_size (Node key value nodel noder size) = size
-
-{-
-turnLeft Nil = Nil
-turnLeft (Node key value nodel noder size) =
-
-turnRight Nil = Nil
-turnRight (Node key value nodel noder size) =
-
-swap Nil = Nil
-swap (Node key value nodel noder size)
-	| get_size nodel - get_size noder > 1 do
-	| get_size noder - get_size nodel > 1 = 
-	| otherwise = (Node key value nodel noder size)
--}
-
-merge Nil (Node key value nodel noder size) = (Node key value nodel noder size)
-merge (Node key value nodel noder size) Nil = (Node key value nodel noder size)
-merge Nil Nil = Nil
-merge (Node keyF valueF nodelF noderF sizeF) 
-	(Node keyS valueS nodelS noderS sizeS) = 
-		Node keyS valueS (merge (Node keyF valueF nodelF noderF sizeF) nodelS) noderS (sizeF + sizeS)
+	| key < k = lookup k nodel
+	| otherwise = lookup k noder
 
 
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
-insert k v Nil = Node k v Nil Nil 1
-insert k v (Node key value nodel noder size)
-	| key == k = Node key v nodel noder size
-	| key < k = Node key value (insert k v nodel) noder ((get_size nodel) + (get_size noder) + 1)
-	| otherwise = Node key value nodel (insert k v noder) ((get_size nodel) + (get_size noder) + 1)
+insert k v Nil = Node k v Nil Nil
+insert k v (Node key value nodel noder)
+	| key == k = Node key v nodel noder
+	| key < k = Node key value (insert k v nodel) noder
+	| otherwise = Node key value nodel (insert k v noder)
+
+
+merge Nil Nil = Nil
+merge Nil (Node key value nodel noder) = Node key value nodel noder
+merge (Node key value nodel noder) Nil = Node key value nodel noder
+merge (Node keyF valueF nodelF noderF) (Node keyS valueS nodelS noderS) = 
+		Node keyS valueS (Node keyF valueF nodelF noderF) $ merge nodelS noderS
 
 
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
 delete k Nil = Nil
-delete k (Node key value nodel noder size)
+delete k (Node key value nodel noder)
 	| key == k = merge nodel noder
-	| key < k = Node key value (delete k nodel) noder ((get_size nodel) + (get_size noder) + 1)
-	| otherwise = Node key value nodel (delete k noder) ((get_size nodel) + (get_size noder) + 1)
+	| key < k = Node key value (delete k nodel) noder
+	| otherwise = Node key value nodel (delete k noder)
